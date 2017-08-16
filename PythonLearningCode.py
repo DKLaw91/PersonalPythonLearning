3c0cd6280f78c941f677878cbf17aaaa200bf227
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-08-16 15:38:45 +0100

    Update PythonLearningCode.py

--- PythonLearningCode.py
+++ PythonLearningCode.py
@@ -1,3 +1,23 @@
+def guessgame():
+  from random import randint
+  secretNumber = randint(1, 20)
+  print('I am thinking of a number between 1 and 20. You have 6 guesses.')
+
+  for guessesTaken in range(1, 7):
+      guess = int(input('Take a guess.'))
+
+      if guess < secretNumber:
+          print('Your guess is too low.')
+      elif guess > secretNumber:
+          print('Your guess is too high.')
+      else:
+          break
+
+  if guess == secretNumber:
+      print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+  else:
+      print('Nope. The number I was thinking of was ' + str(secretNumber))
+
 def jokes():
   from random import randint
   
