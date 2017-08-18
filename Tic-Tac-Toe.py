eb0ecc8120482117272a0caf926c9bd183950333
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-08-18 16:18:47 +0100

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -10,9 +10,9 @@ botR=9
 
 playoptions = [topL, topM, topR, midL, midM, midR, botL, botM, botR]
 
-board = { topL: ' ', topM: ' ', topR: ' ', 
-          midL: ' ', midM: ' ', midR: ' ', 
-          botL: ' ', botM: ' ', botR: ' '}
+board = { topL: '1', topM: '2', topR: '3', 
+          midL: '4', midM: '5', midR: '6', 
+          botL: '7', botM: '8', botR: '9'}
           
 def printBoard(board):
   print(board[topL] + '|' + board[topM] + '|' + board[topR])
@@ -23,27 +23,50 @@ def printBoard(board):
 
 from random import randint
 
+def playerturn():
+  player = playoptions[int(input('where would you like to go?'))]-1
+  if player == 1:
+    board[topL] = 'x'
+  elif player == 2:
+    board[topM] = 'x'
+  elif player == 3:
+    board[topR] = 'x'
+  elif player == 4:
+    board[midL] = 'x'
+  elif player == 5:
+    board[midM] = 'x'
+  elif player == 6:
+    board[midR] = 'x'
+  elif player == 7:
+    board[botL] = 'x'
+  elif player == 8:
+    board[botM] = 'x'
+  else:
+    board[botR] = 'x'
+    
+    
+def cputurn():
+  computer = playoptions[randint(0,8)]
+  print(computer)
+  if computer == 1:
+    board[topL] = 'o'
+  elif computer == 2:
+    board[topM] = 'o'
+  elif computer == 3:
+    board[topR] = 'o'
+  elif computer == 4:
+    board[midL] = 'o'
+  elif computer == 5:
+    board[midM] = 'o'
+  elif computer == 6:
+    board[midR] = 'o'
+  elif computer == 7:
+    board[botL] = 'o'
+  elif computer == 8:
+    board[botM] = 'o'
+  else:
+    board[botR] = 'o'
 
-computer = playoptions[randint(0,8)]
-print(computer)
-if computer == 1:
-  board[topL] = 'o'
-elif computer == 2:
-  board[topM] = 'o'
-elif computer == 3:
-  board[topR] = 'o'
-elif computer == 4:
-  board[midL] = 'o'
-elif computer == 5:
-  board[midM] = 'o'
-elif computer == 6:
-  board[midR] = 'o'
-elif computer == 7:
-  board[botL] = 'o'
-elif computer == 8:
-  board[botM] = 'o'
-else:
-  board[botR] = 'o'
-
-
+cputurn()
+playerturn()
 printBoard(board)
