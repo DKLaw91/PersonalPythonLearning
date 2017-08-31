aad3be8584cbd580a6edeb815db2cc5ab83ad5a8
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-08-31 12:42:40 +0100

    Create Randint.py

---
+++ Randint.py
@@ -0,0 +1,79 @@
+from random import randint
+
+_1  =7
+_2  =11
+_3  =12
+_4  =10
+_5  =10
+_6  =8
+_7  =12
+_8  =6
+_9  =11
+_10 =16
+_11 =12
+_12 =12
+_13 =7
+_14 =10
+_15 =10
+_16 =6
+_17 =15
+_18 =3
+_19 =10
+_20 =17
+_21 =13
+_22 =12
+_23 =11
+_24 =7
+_25 =11
+_26 =10
+_27 =16
+_28 =10
+_29 =10
+_30 =10
+_31 =11
+_32 =5
+_33 =6
+_34 =6
+_35 =9
+_36 =7
+_37 =8
+_38 =10
+_39 =9
+_40 =5
+_41 =9
+_42 =7
+_43 =10
+_44 =12
+_45 =10
+_46 =5
+_47 =11
+_48 =10
+_49 =12
+_50 =8
+
+
+from random import randint
+Three=[18]
+Four=[]
+Five=[32,46]
+Six=[8,16,33,34]
+Seven=[1,13,24,36,42]
+Eight=[6,37,50]
+Nine=[35,39,41,]
+Ten=[4, 5, 14, 15, 19, 26, 28, 29, 30, 38, 43, 45, 48]
+Eleven=[2,9,23,25,31,47]
+Twelve=[3,7,11,12,22,44,49]
+Thirteen=[21,]
+Fourteen=[]
+Fifteen=[17]
+Sixteen=[10,27,]
+Seventeen=[20]
+
+
+
+
+
+print(len(Three+Four+Five+Six+Seven+Eight+Nine+Ten+Eleven+Twelve+Thirteen+Fourteen+Fifteen+Sixteen+Seventeen))
+print(Ten[randint(0,(len(Ten)-1))])
+print(Eleven[randint(0,(len(Eleven)-1))])
+print(Twelve[randint(0, (len(Twelve)-1))])
