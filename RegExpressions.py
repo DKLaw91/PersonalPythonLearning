6dc893720e591d887128e36dd71782d51c030325
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-09-05 16:49:11 +0100

    Update RegExpressions.py

--- RegExpressions.py
+++ RegExpressions.py
@@ -24,3 +24,32 @@ def isPhoneNumber(text):
   return True
 
 isPhoneNumber('4154555-1234')
+
+_________________________________________________________________________________
+
+def isPhoneNumber(text):
+  if len(text) != 12:
+    return False
+  for i in range(0,3):
+    if not text[i].isdecimal():
+      return False
+  if text[3] != '-':
+    return False
+  for i in range(4,7):
+    if not text[1].isdecimal():
+      return False
+  if text[7] != '-':
+    return False
+  for i in range(8,12):
+    if not text[i].isdecimal():
+      return False
+  return True
+
+isPhoneNumber('4154555-1234')
+
+message='Call me at 123-555-0156 tomorrow. 123-555-0538 is my office.'
+for i in range(len(message)):
+  chunk=message[i:i+12]
+  if isPhoneNumber(chunk):
+    print('Phone number found: ' + chunk)
+print('Done.')
