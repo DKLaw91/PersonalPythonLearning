c00bfe4ac31bf2429765bb6e9b2f29f5b50a3784
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-09-06 12:31:31 +0100

    Update RegExpressions.py

--- RegExpressions.py
+++ RegExpressions.py
@@ -59,6 +59,18 @@ import re
 phoneNumRegex=re.compile(r'\d{3}-\d{3}-\d{4}')
 
 mo=phoneNumRegex.search('My number is 415-555-4242.')
-print('Phone number found: ' + mo.group())
-print(mo)
+print('Phone number found: ' + mo[0])
+print(mo[0])
+
+phoneNumRegex=re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+
+mo=phoneNumRegex.search('My number is 415-555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(3))
+print(mo[0])
+
+
+
+
 
