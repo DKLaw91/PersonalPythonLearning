458761211ae95c699a68e513445b6dbec2cc3106
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-10-16 12:29:09 +0100

    Create 161017.py

---
+++ 161017.py
@@ -0,0 +1,59 @@
+int1=7
+print(int1)
+
+float1=7.0
+print(float1)
+
+float1=float(7.00)
+print(float1)
+
+print('##########')
+
+string1='hello'
+print(string1)
+
+string1="hello"
+print(string1)
+
+string2="don't worry about apostrophes"
+print(string2)
+
+string2='don\'t worry about apostrophes'
+print(string2)
+
+print('##########')
+
+print('''I can print
+on multiple lines
+just look here.''')
+
+print('##########')
+
+one=1
+two=2
+three=one + two
+print(three)
+
+hello='hello'
+world='world'
+helloworld=hello + ' ' + world
+print(helloworld)
+
+hello, world = 'hello', 'world'
+print(hello)
+print(world)
+
+print('##########')
+
+mystring=input('string = ')
+myfloat=float(input('float = '))
+myint=int(input('integer = '))
+
+if mystring == 'hello':
+  print('String: ' + mystring)
+if myfloat == 10.00:
+  print('Float: ' + str(myfloat))
+if myint == 20:
+  print('Integer: ' + str(myint))
+
+print('##########')
