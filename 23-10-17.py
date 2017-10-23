e2e7a1e3a1c047896ba3279b53e55ec2b9a81e99
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-10-23 13:58:42 +0100

    Update 23-10-17.py

--- 23-10-17.py
+++ 23-10-17.py
@@ -52,7 +52,7 @@ print()
 print(car2.description())
 _______________________________________________________________
 https://www.learnpython.org/en/Dictionaries
-  
+ 
 phonebook={}
 phonebook["John"]=914065212 # Accepts max of 9 digits!
 phonebook["Jeff"]=87405683
@@ -77,9 +77,25 @@ for name, number in phonebook.items():
   print()
 
 del phonebook["John"]
+phonebook.pop("Jeffald") # Also deletes dictionary item
 
 print(phonebook)
 print()
 
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+
+# write your code here
+phonebook["Jake"]=938273443 # This is how to add to Dictionary
+phonebook.pop("Jill")
+
+# testing code
+if "Jake" in phonebook:
+    print("Jake is listed in the phonebook.")
+if "Jill" not in phonebook:
+    print("Jill is not listed in the phonebook.")
 
 
