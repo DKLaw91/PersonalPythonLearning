ecbf82c639e78bae2062b08cc0864afb90486e52
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-10-26 15:20:36 +0100

    Update 25-10-17.py

--- 25-10-17.py
+++ 25-10-17.py
@@ -8,7 +8,19 @@ pop={"Country": ["Brazil", "Russia", "India", "China", "South Africa"],
 import pandas as pd
 
 brics=pd.DataFrame(pop) # Converts dictionary to Data Frame
+print(brics) # Prints Data Frame
+print()
+brics.index=['1','2','3','4','5'] # Changes index - can be anything
 print(brics)
 print()
-brics.index=[1,2,3,4,5] # Changes index - can be anything
-print(brics)
+print(brics['Capital']) # Prints out individual column as Data Frame
+print()
+print(brics[['Country', 'Capital', 'Area']]) # Prints out selected columns
+print()
+print(brics[0:2]) # Prints first 2 observations (rows)
+print()
+print(brics[2:4]) # Prints 3 & 4th observations (rows)
+print()
+print(brics.iloc[1]) #  Prints out row in reversed format
+print()
+print(brics.loc[['1', '2']]) # Prints 1st & 2nd observations (rows)
