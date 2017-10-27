e9714e5e6925383a8a13f721008b6b6857b3608f
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-10-27 14:35:08 +0100

    Update 27-10-17.py

--- 27-10-17.py
+++ 27-10-17.py
@@ -83,6 +83,34 @@ def bar(first, second, third, **options):
 result=bar(1,2,3,action="sum", number = "first")
 print("Result: " + str(result))
 
+# edit the functions prototype and implementation
+def foo(a, b, c, *d):
+    return len(d)
+
+def bar(a, b, c, **d):
+    if d.get("magicnumber") !=7:
+      return False
+    if d.get("magicnumber")==7:
+      return True
+
+
+# test code
+if foo(1,2,3,4) == 1:
+    print("Good.")
+if foo(1,2,3,4,5) == 2:
+    print("Better.")
+if bar(1,2,3,magicnumber = 6) == False:
+    print("Great.")
+if bar(1,2,3,magicnumber = 7) == True:
+    print("Awesome!")
+
+
+
+
+
+
+
+
 
 
 
