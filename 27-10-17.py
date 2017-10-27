037ad6c1d0d4656610b879132e51391519a74aea
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-10-27 11:23:37 +0100

    Update 27-10-17.py

--- 27-10-17.py
+++ 27-10-17.py
@@ -36,6 +36,28 @@ if type(fib()) == types.GeneratorType:
         counter += 1
         if counter == 10:
             break
+______________________________________________________________
+https://www.learnpython.org/en/List_Comprehensions
+  
+sentence="Good old Jeff, the best of the best."
+words=sentence.split()
+word_lengths=[]
+for word in words:
+  if word != "the":
+    word_lengths.append(len(word))
+print(words)
+print(word_lengths)
+print()
+
+# Alternative to above called List Comprehension
+word_lengths=[len(word) for word in words if word != "the"]
+print(words)
+print(word_lengths)
+
+# Exercise: Using a list comprehension, create a new list called "newlist" out of the list "numbers", which contains only the positive numbers from the list, as integers.
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = [int(number) for number in numbers if number >0]
+print(newlist)
 
 
 
