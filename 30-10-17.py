675c91f095272964ecf394d13fd4c42955c470ad
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-10-30 11:21:48 +0000

    Update 30-10-17.py

--- 30-10-17.py
+++ 30-10-17.py
@@ -23,6 +23,38 @@ def get_last_name():
 get_last_name()
 print("All exceptions caught! Good job!")
 print("The actor's last name is %s." % get_last_name())
+___________________________________________________---
+https://www.learnpython.org/en/Sets
+  
+print(set("My name is Jeff and Jeff is my name".split()))
+print()
+
+a=set(["Jeff", "Jeffery", "Jeffald"])
+print(a)
+b=set(["Jeff", "Jeffaldine"])
+print(b)
+print()
+
+print(a.intersection(b))
+print(b.intersection(a))
+print()
+
+print(a.symmetric_difference(b))
+print(b.symmetric_difference(a))
+print()
+
+print(a.difference(b))
+print(b.difference(a))
+print()
+
+print(a.union(b))
+print()
+
+#In the exercise below, use the given lists to print out a set containing all the participants from event A which did not attend event B.
+
+c = set(["Jake", "John", "Eric"])
+d = set(["John", "Jill"])
+print(c.difference(d))
 
 
 
