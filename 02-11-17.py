74b8c414bb9d304b13ac016c65664cd7ac15493d
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-11-03 13:14:40 +0000

    Update 02-11-17.py

--- 02-11-17.py
+++ 02-11-17.py
@@ -50,3 +50,31 @@ def num(num):
   return num # Will not work with print() as return string not integer
 
 print(num(15))
+print()
+
+# Make a decorator factory which returns a decorator that decorates functions with one argument. The factory should take one argument, a type, and then returns a decorator that makes function should check if the input is the correct type. If it is wrong, it should print("Bad Type". (In reality, it should) raise an error, but error raising isn't in this tutorial.) Look at the tutorial code and expected output to see what it is if you are confused (I know I would be.) Using isinstance(object, type_of_object) or type(object) might help.
+
+def type_check(correct_type):
+    # My Code
+    def newfunc(oldfunc):
+      def checker(num):
+        if isinstance(num, correct_type):
+          return oldfunc(num)
+        else:
+          print("Error: Bad Type")
+      return checker
+    return newfunc
+
+@type_check(int)
+def times2(num):
+    return num*2
+
+print(times2(2))
+times2('Not A Number')
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+first_letter(['Not', 'A', 'String'])
