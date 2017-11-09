8695019be621b055e4a884a2f539dfe888f0c8cf
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2017-11-09 16:04:25 +0000

    Update 06-11-17.py

--- 06-11-17.py
+++ 06-11-17.py
@@ -260,8 +260,39 @@ def loops():
       print(i)
     if i==237:
       break
+###################################################################
+def functions():
+  def names():
+    names=["Thor", "Loki", "Odin", "Hela"]
+    for i in names:
+      print(i)
+    print()
+  names()
+  def slapped(slapper, slappee):
+    print(slapper + " pwopah slapt " + slappee + ".")
+    print()
+  slapped("Valkyrie", "Loki")
+  def sum(a,b):
+    return a+b
+    print()
+  print(sum(12,15))
   
-loops()
+  # Modify this function to return a list of strings as defined above
+  def list_benefits():
+      return ["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]
+      
+  
+  # Modify this function to concatenate to each benefit - " is a benefit of functions!"
+  def build_sentence(benefit):
+      print(benefit + " is a benefit of functions")
+  
+  def name_the_benefits_of_functions():
+      list_of_benefits = list_benefits()
+      for benefit in list_of_benefits:
+          print(build_sentence(benefit))
+  
+  name_the_benefits_of_functions()
+###############################
 
 
 
