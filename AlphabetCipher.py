466e721f78e2ea9b0e881932369b6bead2594d46
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2018-04-23 15:55:06 +0100

    Update AlphabetCipher.py

--- AlphabetCipher.py
+++ AlphabetCipher.py
@@ -1,12 +1,12 @@
 Alphabet=["abcdefghijklmnopqrstuvwxyz"]
 string=Alphabet[0]
-#Popoulates Alphabet#
+#Populates Alphabet#
 for Row in range(1,26):
     first=string[0]
     string=string[1:]
     string=string+first
     Alphabet.append(string)
-#Popoulates Alphabet#
+#Populates Alphabet#
     
 #Prints Alphabet#    
 def printAlphabet(Alphabet):
@@ -18,7 +18,7 @@ def printAlphabet(Alphabet):
     
 codeDict={
     }
-#Popoulates Dictionary with letters of alphabet matched to numbers#
+#Populates Dictionary with letters of alphabet matched to numbers#
 def populateDict(dict):
     count=0
     for number in range(0,26):
@@ -28,7 +28,7 @@ def populateDict(dict):
 def printDictionary(dict):
     for key, value in sorted(dict.items()):
         print(key + ":" + str(value))
-#Popoulates Dictionary with letters of alphabet matched to numbers#
+#Populates Dictionary with letters of alphabet matched to numbers#
 
 populateDict(codeDict)
 
