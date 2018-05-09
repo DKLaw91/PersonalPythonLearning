9cb34761feadead86e34e0dbfe312453147f3b42
Time2Mire <31003081+Time2Mire@users.noreply.github.com>
2018-05-09 15:39:13 +0100

    Update pythonChallenges.py

--- pythonChallenges.py
+++ pythonChallenges.py
@@ -678,3 +678,12 @@ for n in ret:
 print(",".join(retlist))
 
 70.
+Question:
+
+
+Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+
+
+
+Hints:
+Use "assert expression" to make assertion.
